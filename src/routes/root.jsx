81c521cbd8e7893d815b1f5597990519a92051cb
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-04-15 16:29:53 +0800

    feat: add root route element

---
+++ src/routes/root.jsx
@@ -0,0 +1,25 @@
+export default function Root() {
+  return (
+    <>
+      <header>
+        <a href="#">
+          <h1>Amaze</h1>
+        </a>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+            <li>
+              <a href="#">Cart</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main></main>
+    </>
+  );
+}
