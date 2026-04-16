e5b150d7318feb6997b25c9637fbcc4739909bf2
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-04-16 17:18:20 +0800

    feat: add home data

---
+++ src/home-data.js
@@ -0,0 +1,28 @@
+const homeData = [
+  {
+    title: 'Up to 95% off on your first 3 orders',
+    imageUrl: 'src/assets/images/christopher-gower-_aXa21cf7rY-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Download app',
+  },
+  {
+    title: 'Free shipping',
+    imageUrl: 'src/assets/images/marissa-grootes-D4jRahaUaIc-unsplash.jpg',
+    linkUrl: 'shop',
+    linkText: 'Shop now',
+  },
+  {
+    title: 'Lorem ipsum',
+    imageUrl: 'src/assets/images/riyan-ong-j1PxAa2U-T4-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Dolor sit amet',
+  },
+  {
+    title: 'Ed do eiusmod tempor incididunt',
+    imageUrl: 'src/assets/images/sayan-majhi-rZDBOGPHU7w-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Ut labore et',
+  },
+];
+
+export default homeData;
