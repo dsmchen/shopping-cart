4e3ec0f8b60289dd3000eb4259da738ecff34cc9
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-04-17 12:44:20 +0800

    feat: add home and home card responsive CSS

--- src/home-data.js
+++ src/home-data.js
@@ -23,6 +23,54 @@ const homeData = [
     linkUrl: '',
     linkText: 'Ut labore et',
   },
+  {
+    title: 'Up to 95% off on your first 3 orders',
+    imageUrl: 'src/assets/images/christopher-gower-_aXa21cf7rY-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Download app',
+  },
+  {
+    title: 'Free shipping',
+    imageUrl: 'src/assets/images/marissa-grootes-D4jRahaUaIc-unsplash.jpg',
+    linkUrl: 'shop',
+    linkText: 'Shop now',
+  },
+  {
+    title: 'Lorem ipsum',
+    imageUrl: 'src/assets/images/riyan-ong-j1PxAa2U-T4-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Dolor sit amet',
+  },
+  {
+    title: 'Ed do eiusmod tempor',
+    imageUrl: 'src/assets/images/sayan-majhi-rZDBOGPHU7w-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Ut labore et',
+  },
+  {
+    title: 'Up to 95% off on your first 3 orders',
+    imageUrl: 'src/assets/images/christopher-gower-_aXa21cf7rY-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Download app',
+  },
+  {
+    title: 'Free shipping',
+    imageUrl: 'src/assets/images/marissa-grootes-D4jRahaUaIc-unsplash.jpg',
+    linkUrl: 'shop',
+    linkText: 'Shop now',
+  },
+  {
+    title: 'Lorem ipsum',
+    imageUrl: 'src/assets/images/riyan-ong-j1PxAa2U-T4-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Dolor sit amet',
+  },
+  {
+    title: 'Ed do eiusmod tempor',
+    imageUrl: 'src/assets/images/sayan-majhi-rZDBOGPHU7w-unsplash.jpg',
+    linkUrl: '',
+    linkText: 'Ut labore et',
+  },
 ];
 
 export default homeData;
