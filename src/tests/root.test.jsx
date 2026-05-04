64d25634e8e223b581cd97072c20d71ef08cefcd
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-05-04 17:06:54 +0800

    test: rewrite root test file

--- src/tests/root.test.jsx
+++ src/tests/root.test.jsx
@@ -1,27 +1,64 @@
-import { MemoryRouter } from 'react-router-dom';
+import { createRoutesStub } from 'react-router-dom';
 import { describe, it, expect, beforeEach } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Root from '../routes/root';
+import Home from '../routes/home/home';
+import Shop from '../routes/shop/shop';
+import Cart from '../routes/cart/cart';
 
 beforeEach(() => {
-  render(
-    <MemoryRouter>
-      <Root />
-    </MemoryRouter>,
-  );
+  const Stub = createRoutesStub([
+    {
+      path: '/',
+      Component: Root,
+      children: [
+        {
+          index: true,
+          Component: Home,
+        },
+        {
+          path: '/shop',
+          Component: Shop,
+        },
+        {
+          path: '/cart',
+          Component: Cart,
+        },
+      ],
+    },
+  ]);
+
+  render(<Stub />);
 });
 
 describe('Root component', () => {
   it('renders correct heading', () => {
-    expect(screen.getByRole('heading').textContent).toMatch(/amaze/i);
+    expect(screen.getByRole('heading', { level: 1 }).textContent).toMatch(
+      /amaze/i,
+    );
   });
 
   it('navigates correctly', async () => {
-    const user = userEvent.setup();
+    const homeHeadings = screen.getAllByRole('heading', {
+      name: 'Up to 95% off on your first 3 orders',
+      level: 2,
+    });
 
-    expect(
-      await screen.findByText(/up to 95% off on your first 3 orders/i),
-    ).toBeInTheDocument();
+    expect(homeHeadings).toHaveLength(3);
+
+    userEvent.click(screen.getByText('Shop'));
+    await waitFor(() => screen.findByText('Loading...'));
+
+    userEvent.click(screen.getByText('Amaze'));
+    expect(homeHeadings).toHaveLength(3);
+
+    userEvent.click(screen.getByText('Cart (0)'));
+    await waitFor(() =>
+      screen.getByRole('heading', { name: 'Your cart is empty', level: 2 }),
+    );
+
+    userEvent.click(screen.getByText('Home'));
+    expect(homeHeadings).toHaveLength(3);
   });
 });
