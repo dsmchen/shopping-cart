56ea5fe251b3de08c1b2a76e79def5947ec6f8bf
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-05-05 13:47:24 +0800

    test: add renders data state test to shop file

--- src/tests/shop.test.jsx
+++ src/tests/shop.test.jsx
@@ -1,5 +1,7 @@
+import { createRoutesStub } from 'react-router-dom';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import Root from '../routes/root';
 import Shop from '../routes/shop/shop';
 
 describe('Shop component', () => {
@@ -12,8 +14,39 @@ describe('Shop component', () => {
   it('renders error state', async () => {
     render(<Shop propUrl="https://example.com/" />);
 
-    await screen.findByRole('heading', undefined, { timeout: 9000 });
+    await screen.findByRole('heading', { name: 'Oops!' });
 
     expect(screen.getByRole('heading').textContent).toMatch(/oops/i);
+  });
+
+  it('renders data state', async () => {
+    const Stub = createRoutesStub([
+      {
+        path: '/',
+        Component: Root,
+        children: [
+          {
+            path: '/shop',
+            Component: Shop,
+          },
+        ],
+      },
+    ]);
+
+    render(<Stub initialEntries={['/shop']} />);
+
+    await screen.findByRole(
+      'heading',
+      {
+        name: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+      },
+      { timeout: 5000 },
+    );
+
+    expect(
+      screen.getAllByRole('heading', {
+        level: 2,
+      }),
+    ).toHaveLength(20);
   });
 });
