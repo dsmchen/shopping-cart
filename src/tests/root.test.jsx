65ead778bf66c5b9599198f902a3b5c3ab0bc926
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-05-06 11:10:39 +0800

    test: refactor root test

--- src/tests/root.test.jsx
+++ src/tests/root.test.jsx
@@ -1,6 +1,6 @@
 import { createRoutesStub } from 'react-router-dom';
 import { describe, it, expect, beforeEach } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Root from '../routes/root';
 import Home from '../routes/home/home';
@@ -42,24 +42,21 @@ describe('Root component', () => {
   it('navigates correctly', async () => {
     const homeHeadings = screen.getAllByRole('heading', {
       name: 'Up to 95% off on your first 3 orders',
-      level: 2,
     });
     const user = userEvent.setup();
 
     expect(homeHeadings).toHaveLength(3);
 
-    user.click(screen.getByText('Shop'));
-    await waitFor(() => screen.findByText('Loading...'));
+    await user.click(screen.getByRole('link', { name: 'Shop' }));
+    screen.getByRole('heading', { name: 'Loading...' });
 
-    user.click(screen.getByText('Amaze'));
+    await user.click(screen.getByRole('link', { name: 'Amaze' }));
     expect(homeHeadings).toHaveLength(3);
 
-    user.click(screen.getByText('Cart (0)'));
-    await waitFor(() =>
-      screen.getByRole('heading', { name: 'Your cart is empty', level: 2 }),
-    );
+    await user.click(screen.getByRole('link', { name: 'Cart (0)' }));
+    screen.getByRole('heading', { name: 'Your cart is empty' });
 
-    user.click(screen.getByText('Home'));
+    await user.click(screen.getByRole('link', { name: 'Home' }));
     expect(homeHeadings).toHaveLength(3);
   });
 });
