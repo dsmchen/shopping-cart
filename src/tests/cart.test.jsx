938f60540a041f2e4fc760c87981d67d96bd99b8
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-05-06 11:16:45 +0800

    test: refactor cart test

--- src/tests/cart.test.jsx
+++ src/tests/cart.test.jsx
@@ -1,28 +1,26 @@
 import { createRoutesStub } from 'react-router-dom';
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Root from '../routes/root';
 import Cart from '../routes/cart/cart';
 
-beforeEach(() => {
-  const Stub = createRoutesStub([
-    {
-      path: '/',
-      Component: Root,
-      children: [
-        {
-          path: '/cart',
-          Component: Cart,
-        },
-      ],
-    },
-  ]);
-
-  render(<Stub initialEntries={['/cart']} />);
-});
-
 describe('Cart component', () => {
   it('renders empty cart state', () => {
+    const Stub = createRoutesStub([
+      {
+        path: '/',
+        Component: Root,
+        children: [
+          {
+            path: '/cart',
+            Component: Cart,
+          },
+        ],
+      },
+    ]);
+
+    render(<Stub initialEntries={['/cart']} />);
+
     expect(screen.getByRole('heading', { level: 2 }).textContent).toMatch(
       /your cart is empty/i,
     );
